615887aed287386f9416b8bc9394bdef67b12691
10709 <11035097@fhsh.tp.edu.tw>
2022-05-06 11:58:51 +0800

    基本版型完成!

--- src/main.js
+++ src/main.js
@@ -4,4 +4,13 @@ $(document).ready(functoin () {
   $("#sign-in-form").submit(function (e) {
     e.preventDefault();
   });
+
+  //TOOD: 讓帳號密碼輸入時，版型維持
+  $("#account").blur( funtion (e) {
+    if ($(e.traget).val !== "") {
+      $("#addr-hint-text").addClass("");
+    }
+  });
+
+  $("password");
 });
